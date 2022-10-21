53b24723f28ae246385fdc5d60f01491058856fb
Gerome <geromeandrew7@gmail.com>
2022-10-21 21:58:59 +0800

    Setup routing and sample object detection component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+import Sample from "./models/sample/Sample";
+
 function App() {
   return (
     <div className="App">
       <h1>HAILS - HANDSIGN ARTIFICIAL INTELLIGENCE LEARNING SYSTEM</h1>
+      <Routes>
+        <Route path="/" element={null} />
+        <Route path="/sample" element={<Sample />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/models/sample/SampleUtilities.js
@@ -0,0 +1,44 @@
+// Define our labelmap
+// const labelMap = {
+//     1:{name:'A', color:'red'},
+//     2:{name:'B', color:'yellow'},
+//     3:{name:'C', color:'lime'},
+//     4:{name:'D', color:'blue'},
+//     5:{name:'E', color:'purple'},
+//     6:{name:'Hello', color:'red'},
+//     7:{name:'I Love You', color:'yellow'},
+//     8:{name:'Thank You', color:'lime'},
+//     9:{name:'Yes', color:'blue'},
+//     10:{name:'No', color:'purple'}
+// }
+
+const labelMap = {
+    1:{name:'Hello', color:'red'},
+    2:{name:'Thank You', color:'yellow'},
+    3:{name:'I Love You', color:'lime'},
+    4:{name:'Yes', color:'blue'},
+    5:{name:'No', color:'purple'},
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'white'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.stroke()
+        }
+    }
+}
