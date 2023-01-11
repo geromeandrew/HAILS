e727c600438988fd96ebdd69f621f4955161167d
slrhc <charlesporazo23@gmail.com>
2023-01-11 18:58:08 +0800

    Update popup.js

--- frontend/src/components/popup.js
+++ frontend/src/components/popup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../assets/styles/Popup.css'
 import { Link } from 'react-router-dom'
-import Question from '../components/Questionnaire'
+import Question from './Questionnaire'
 
 function Popup() {
 
